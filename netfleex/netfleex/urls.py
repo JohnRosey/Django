e629d8acd586590b81eb47ecf5831401b99450bf
Gansonre Ismael <proismaelgans2017@gmail.com>
2021-12-18 11:38:30 -0500

    Ajout de l'user part1

--- netfleex/netfleex/urls.py
+++ netfleex/netfleex/urls.py
@@ -1,9 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
+from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),# le chemin qui est dans path '' renvoie vers le fichier urls.py du dossier core
-
+    path('register/',user_views.register, name='register'),
 ]
