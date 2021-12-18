f548f0f79f9adf6b8c7df869a035edea8661565d
Gansonre Ismael <proismaelgans2017@gmail.com>
2021-12-18 07:41:39 -0500

    update

--- netfleex/core/admin.py
+++ netfleex/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Category, Netfeelex
+
 # Register your models here.
+admin.site.register(Netfeelex)
+admin.site.register(Category)
--- netfleex/core/models.py
+++ netfleex/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.db.models.fields import DateTimeField
+from datetime import datetime
 
 # Create your models here.
+class Netfeelex(models.Model):
+    #CharFiels pour mettre une restriction
+    Titre_film = models.CharField(max_length=200)
+    Contenu_film = models.TextField()
+    Date_publication = models.DateTimeField("Date publié", default = datetime.now())
+    Category = models.CharField(max_length=200)
+    #fonction de recuperation
+    def __str__(self):
+        return self.Titre_film
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+"""class User(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    email = models.EmailField()
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.username"""
