2442afec38f747c58fef1819a555caa9fa9eec0c
Gansonre Ismael <proismaelgans2017@gmail.com>
2021-12-19 01:43:14 -0500

    arrive a enregistrer un user

--- netfleex/users/models.py
+++ netfleex/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from core.models import Category
+from core.models import Netfeelex
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    email = models.EmailField()
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.username
--- netfleex/users/views.py
+++ netfleex/users/views.py
@@ -1,14 +1,46 @@
-from django import forms
+from random import randint
+from typing import Reversible
+from django.db.models.expressions import Random
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .models import Netfeelex
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
+from .forms import newUser
+from django.contrib.auth.forms import UserCreationForm
+
+
+
 # Create your views here.
 def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+    """Register a new user."""
+    if request.method != 'POST':
+        # Display blank registration form.   
+        form = UserCreationForm()
+    else:
+        # Process completed form.
+        form = UserCreationForm(data=request.POST)
+        
         if form.is_valid():
-            username=form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            new_user = form.save()
+            # Log the user in, and then redirect to home page.
+            authenticated_user = authenticate(username=new_user.username,
+                password=request.POST['password1'])
+            login(request, authenticated_user)
+            
             return redirect('home')
-    form=UserCreationForm()
-    return render(request, 'register.html', {'form': UserCreationForm})
+
+    context = {'form': form}
+    return render(request, 'register.html', context)
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Session est fermée")
+    return redirect("home")
+
+def user(request):
+    profile = request.user.get_profile()
+    i = randint(0,10)
+    
+    return redirect("/user.html")
